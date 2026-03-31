bbcdbe9d7b07a5ed6807ebcb095a0095e1e59a9b
Erblin Sylejmani <erindmusliu.11@gmail.com>
2026-03-31 18:45:49 +0200

    Kam krijuar perfunduar folderin Task.php

--- classes/Task.php
+++ classes/Task.php
@@ -4,20 +4,41 @@ include_once('Database.php');
 
 class Task extends Database{
     public function getAllTasks(){
-       $sql="select * from tasks";
+       $sql="select * from detyrat";
 
        return $this->conn->query($sql);
        
     }
 
     public function getTaskById($id){
-        $sql = "select * from tasks where id='$id'";
+        $sql = "select * from detyrat where id='$id'";
 
         return $this->conn->query($sql);
        
     }
 
-    public function addTask(){
-        $sql = "insert * into tasks";
+    public function addTask($emri_detyres,$data_fillimit,$data_perfundimit,$change_status){
+        $sql = "insert * into detyrat(emri_detyres,data_fillimit,data_perfundimit,change_status) value('$emri_detyres','$data_fillimit','$data_perfundimit','$change_status')";
+
+        return $this->conn->query($sql);
+    }
+
+    public function updateTask($emri_detyres,$data_fillimit,$data_perfundimit,$change_status){
+        $sql = "update detyrat set emri_detyres='$emri_detyres',data_fillimit='$data_fillimit',data_perfundimit='$data_perfundimit'";
+
+        return $this->conn->query($sql);
+    }
+
+    public function deleteTask($id){
+        $sql = "delete from detyrat where id='$id'";
+
+        return $this->conn->query($sql);
+    }
+
+    public function changeStatus($change_status){
+       $sql = "update from detyrat where change_status='$change_status'";
+
+       return $this->conn->query($sql);
+
     }
 }
